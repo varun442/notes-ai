4f6ae971e01ee5c8eaa1eb26a4b039300d885ca4
Varun Savai <varun.savai23@gmail.com>
2024-07-30 22:22:06 -0400

    Update Node.js engine to 20.x

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
+const connectionString = process.env.MONGODB_URI;
 
-const config = require("./config.json");
 const mongoose = require("mongoose")
 
-mongoose.connect(config.connectionString);
+mongoose.connect(connectionString);
 
 const User = require("./models/user.model")
 const Note = require("./models/note.model")
